1f5c9a273b15fe5b67887e56967572aae1523b86
Twynzen <razzziel001@gmail.com>
2019-10-21 23:11:02 -0500

    Practicando formulario ahora si sirve 100% real no fake

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+  //conectamos con el servidor
+  $conectar=mysql_connect('databases.000webhost.com', 'acaelusuario', 'acalacontrasena')
+  //verificamos la MongoWriteConcernException
+  if (!$conectar) {
+    echo "No se conecto te he faladoooo ";
+  }else{
+    $base=mysql_select_db('');
+    if (!$base) {
+      echo "No se encontro la base de datos";
+    }
+  }
+  //recuperar las variables
+  $nombre=$_POST['nombre'];
+  $edad=$_POST['edad'];
+  $correo=$_POST['correo'];
+  //hacemos la sentencia sql
+  $sql="INSERT INTO datos VALUES ('$nombre', '$edad', '$correo')";
+  //ejecutamos la sentencia sql
+  
+
+
+ ?>
